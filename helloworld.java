58a8ceb147429c7b78fe2b80b65a91c0c5b5fdf9
achawla2 <92425237+achawla2@users.noreply.github.com>
2023-01-05 17:21:21 -0800

    commit 1

--- helloworld.java
+++ helloworld.java
@@ -1,27 +1,8 @@
 class helloworld{  
     public static void main(String args[]){  
-     System.out.println("Hello Java 123445");  
-     System.out.println("Hello Java 123445");  
-          System.out.println("Hello Java 123445 jan 5");  
-        
-        
 
-          System.out.println("Hello Java 123445 jan 5 back to vsc");  
-          System.out.println("Hello Java 123445 jan 5 back to github");  
-                  System.out.println("Hello Java 123445 jan 5 back to vsc");                    System.out.println("Hello Java 123445 jan 5 back to vsc");  
-
-                  System.out.println("Anisha   jhhhh Hello Java 123445 jan 5 back to vsc");                    System.out.println("Hello Java 123445 jan 5 back to vsc");  
-
-
-                  System.out.println("Hello Java 123445 jan 5 back to vsc");  
-                  System.out.println("Hello Java 123445 jan 5 back to vsc");  
-                  System.out.println("Anisha Hello Java 123445 jan 5 back to vsc");  
-
-                      System.out.println("to aradhika laptopmc");  
-
-                      System.out.println("to aradhanishihlkh ika laptopmc");  
-
-
+        System.out.println("Anisha pushing test 1");  
+        System.out.println("Test 2");  
 
     }  
 }  
